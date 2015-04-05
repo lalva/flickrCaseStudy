c4c9963ee517f2401c5f2d4cab208ed064557895
vanik08 <vanik08@gmail.com>
2015-04-04 21:56:29 -0700

    Added Material Design Library

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,13 @@
  * Controller of the flickerCaseStudyApp
  */
 angular.module('flickerCaseStudyApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $mdSidenav) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    $scope.toggleSidenav = function(menuId) {
+      $mdSidenav(menuId).toggle();
+    };
   });
