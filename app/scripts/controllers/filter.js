50f619427f69ef91b7db69b690997612f93039a4
vanik08 <vanik08@gmail.com>
2015-04-05 19:21:06 -0700

    Clean up of extra controller

--- app/scripts/controllers/filter.js
+++ app/scripts/controllers/filter.js
@@ -9,6 +9,7 @@
  */
 angular.module('flickerCaseStudyApp')
   .controller('FilterCtrl', function ($scope) {
+  	var vm = this;
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
